3dd53f267c3ce594a2e3e9b9f3279f021ff0d44f
James Page <james.page@ubuntu.com>
2022-09-27 09:53:46 +0100

    [chore] add copyright notices.

--- tests/test_question.py
+++ tests/test_question.py
@@ -1,3 +1,17 @@
+# Copyright 2022 Canonical Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import sys
 import os
 import unittest
@@ -67,10 +81,8 @@ class GoodStringQuestion(Question):
 
 
 class TestQuestionClass(unittest.TestCase):
-    """
-    Test basic features of the Question class.
+    """Test basic features of the Question class."""
 
-    """
     def test_invalid_type(self):
 
         with self.assertRaises(InvalidQuestion):
@@ -89,12 +101,10 @@ class TestQuestionClass(unittest.TestCase):
 
 
 class TestInput(unittest.TestCase):
-    """
-    Test input handling.
+    """Test input handling.
 
     Takes advantage of the fact that we can override the Question
     class's input handler.
-
     """
     @mock.patch('microstack_init.questions.question.shell.check_output')
     @mock.patch('microstack_init.questions.question.shell.check')
